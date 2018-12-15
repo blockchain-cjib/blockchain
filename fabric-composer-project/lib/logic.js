cf4aefde2226ae4cde3a624bc46172b699aeef0a
Yzoni <mail@yorickdeboer.nl>
2018-12-15 11:35:34 +0100

    Fabric init cjib check transaction

--- fabric-composer-project/lib/logic.js
+++ fabric-composer-project/lib/logic.js
@@ -3,3 +3,12 @@ async function uploadInfo(creation){
     getAssetRegistry(org.example.cjibnetwork.PersonInfo);
     await assetRegistry.update(creation.personinfo);
 }
+
+async function cjibGetPersonInfo(tx) {
+
+    let citizen = await query('getCitizen', {bsnParam: tx.bsn})[0];
+
+    let citizenSalary = citizen.salary;
+
+    return citizenSalary > tx.fine;
+}
