34be4f879a1e96134737827078ba0f3f0df5c125
P1ranhas <agg_gam@hotmail.com>
2019-01-16 05:37:37 +0100

    Cjib-app: Visual updates

--- cjib-app/src/app/login/login.component.ts
+++ cjib-app/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {Router} from '@angular/router';
 
 @Component({
@@ -7,14 +8,30 @@ import {Router} from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  submitted = false;
 
-  email = '';
-  password = '';
-
-  constructor(private router: Router) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router) {
   }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+  
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.loginForm.invalid) {
+        return;
+    }
+    this.login();
   }
 
   login = function () {
