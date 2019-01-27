2ddd1d66603b22a61dec89e735cb98cf172aa71b
Angelos Gamvrinos <agg_gam@hotmail.com>
2019-01-27 02:42:38 +0100

    Fix mun-app unit tests

--- municipalities-app/src/app/login/login.component.spec.ts
+++ municipalities-app/src/app/login/login.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from '../app-routing.module';
+import { HttpClientModule }    from '@angular/common/http';
 
 import { LoginComponent } from './login.component';
 import { CitizenFormComponent } from '../citizen-form/citizen-form.component';
@@ -15,6 +16,7 @@ describe('LoginComponent', () => {
       imports: [
         AppRoutingModule,
         FormsModule,
+        HttpClientModule,
         ReactiveFormsModule
      ],
       declarations: [
